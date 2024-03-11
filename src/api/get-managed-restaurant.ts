c75ba922f44fe127cb0884a0aa2bd3396694d5df
geraldojalves <junior.alves.2007@gmail.com>
2024-03-11 19:10:59 -0300

    feat: api restaurant managed

---
+++ src/api/get-managed-restaurant.ts
@@ -0,0 +1,18 @@
+import { api } from '@/lib/axios'
+
+export interface GetManagedRestaturantResponse {
+  id: string
+  name: string
+  description: string
+  managerId: string
+  createdAt: Date | null
+  updatedAt: Date | null
+}
+
+export async function getManagedRestaturant() {
+  const response = await api.get<GetManagedRestaturantResponse>(
+    '/managed-restaturant',
+  )
+
+  return response.data
+}
