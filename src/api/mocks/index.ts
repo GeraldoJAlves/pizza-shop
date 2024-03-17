0bc9092e629e62502f4899981f13c0597d8dc512
Geraldo Junior <junior.alves.2007@gmail.com>
2024-03-17 10:25:43 -0300

    test: mock get day orders amount

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -3,14 +3,18 @@ import { setupWorker } from 'msw/browser'
 import { env } from '@/env'
 
 import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount'
 import { signInMock } from './sign-in-mock'
 
-export const worker = setupWorker(signInMock, getDailyRevenueInPeriodMock)
+export const worker = setupWorker(
+  signInMock,
+  getDailyRevenueInPeriodMock,
+  getDayOrdersAmountMock,
+)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
     return
   }
-  console.log('test')
   await worker.start()
 }
