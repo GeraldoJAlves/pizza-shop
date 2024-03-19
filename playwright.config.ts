35bd7a00da06af5d60d0b4a815124ac85d5cd306
Geraldo Junior <junior.alves.2007@gmail.com>
2024-03-19 18:38:12 -0300

    chore: config playwright

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './test',
+  fullyParallel: true,
+  forbidOnly: !process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  use: {
+    baseURL: 'http://localhost:50789',
+  },
+  webServer: {
+    command: 'npm run dev:test',
+    url: 'http://localhost:50789',
+    reuseExistingServer: !process.env.CI,
+  },
+})
