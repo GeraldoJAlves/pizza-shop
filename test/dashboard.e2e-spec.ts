e368c8e67d7b5434af96bc05530552a51e917cd3
Geraldo Junior <junior.alves.2007@gmail.com>
2024-03-20 10:36:53 -0300

    test: display month canceled orders amount metric

--- test/dashboard.e2e-spec.ts
+++ test/dashboard.e2e-spec.ts
@@ -8,3 +8,12 @@ test('display day orders amount metric', async ({ page }) => {
     page.getByText('-5% em relação a ontem', { exact: true }),
   ).toBeVisible()
 })
+
+test('display month canceled orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(page.getByText('5', { exact: true })).toBeVisible()
+  await expect(
+    page.getByText('-5% em relação ao mês passado', { exact: true }),
+  ).toBeVisible()
+})
