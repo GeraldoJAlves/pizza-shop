bac7f06a2ee01d19011f9dd6f4ac0a153b167c41
Geraldo Junior <junior.alves.2007@gmail.com>
2024-03-19 22:12:46 -0300

    test: sign in with wrong credentials

--- test/sign-in.e2e-spec.ts
+++ test/sign-in.e2e-spec.ts
@@ -10,3 +10,12 @@ test('sign in successfully', async ({ page }) => {
   )
   await expect(toast).toBeVisible()
 })
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+  await page.getByLabel('Seu e-mail').fill('wrong@example.com')
+  await page.getByRole('button', { name: 'Acessar painel' }).click()
+
+  const toast = page.getByText('Credenciais inválidas.')
+  await expect(toast).toBeVisible()
+})
