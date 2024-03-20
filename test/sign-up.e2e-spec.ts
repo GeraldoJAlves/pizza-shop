9e95e293f56f725e08201a620e6a49be4dc3fb26
Geraldo Junior <junior.alves.2007@gmail.com>
2024-03-19 22:30:18 -0300

    test: navigate to login page

--- test/sign-up.e2e-spec.ts
+++ test/sign-up.e2e-spec.ts
@@ -14,3 +14,26 @@ test('sign up successfully', async ({ page }) => {
 
   await expect(toast).toBeVisible()
 })
+
+test('sign up with error', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Nome do estabelecimento').fill('Invalid name')
+  await page.getByLabel('Seu nome').fill('John Doe')
+  await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+  await page.getByLabel('Seu celular').fill('12345678990')
+
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Erro ao cadastrar restaurante')
+
+  await expect(toast).toBeVisible()
+})
+
+test('navigate to login page', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Fazer login' }).click()
+
+  await expect(page.url()).toContain('/sign-in')
+})
