a4418d8208bd6fa0e4100051bf1f21e1eda2b18a
Geraldo Junior <junior.alves.2007@gmail.com>
2024-03-20 10:32:58 -0300

    test: display day orders amount

---
+++ test/dashboard.e2e-spec.ts
@@ -0,0 +1,10 @@
+import { expect, test } from '@playwright/test'
+
+test('display day orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(page.getByText('20', { exact: true })).toBeVisible()
+  await expect(
+    page.getByText('-5% em relação a ontem', { exact: true }),
+  ).toBeVisible()
+})
