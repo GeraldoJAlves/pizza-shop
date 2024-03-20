d657e80091eafc428f776b1fcd5ed4e9b3705bcd
Geraldo Junior <junior.alves.2007@gmail.com>
2024-03-20 10:38:17 -0300

    test: display month revenue metric

--- test/dashboard.e2e-spec.ts
+++ test/dashboard.e2e-spec.ts
@@ -26,3 +26,12 @@ test('display month canceled orders amount metric', async ({ page }) => {
     page.getByText('-5% em relação ao mês passado', { exact: true }),
   ).toBeVisible()
 })
+
+test('display month revenue metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(page.getByText('R$ 200,00', { exact: true })).toBeVisible()
+  await expect(
+    page.getByText('+10% em relação ao mês passado', { exact: true }),
+  ).toBeVisible()
+})
