74b13bad988bac6d96f7ce8d68c517ac15b247cd
Geraldo Junior <junior.alves.2007@gmail.com>
2024-03-19 22:08:53 -0300

    test: sign in end to end

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -36,7 +36,7 @@ export function SignIn() {
   async function handleSignIn(data: SignInForm) {
     try {
       await authenticate({ email: data.email })
-      toast.success('Enviamos um link de autenticação para seu email.', {
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
           label: 'Reenviar',
           onClick: () => {
